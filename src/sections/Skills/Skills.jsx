e234fd2508c5e4da3fe2e7e1d6134c3668732f5c
Zaharias <zaharias.polytseris@gmail.com>
2025-04-15 15:30:58 +0300

    Update Skills.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -7,31 +7,24 @@ function Skills() {
         <section id="Skills" className={styles.container}>
             <h1 className='sectionTitle'>Skills</h1>
             <div className={styles.skillList}>
-                <SkillList src={checkMarkIcon} skill="Java"/>
-                <SkillList src={checkMarkIcon} skill="Spring Boot"/>
-                <SkillList src={checkMarkIcon} skill="Node.js"/>
-                <SkillList src={checkMarkIcon} skill="Express.js"/>
+                <SkillList src={checkMarkIcon} skill="Apex (OOP, DML, Logic)"/>
+                <SkillList src={checkMarkIcon} skill="SOQL (Salesforce Query Language)"/>
+                <SkillList src={checkMarkIcon} skill="Apex Triggers"/>
+                <SkillList src={checkMarkIcon} skill="Apex Unit Testing"/>
             </div>
             <hr />
             <div className={styles.skillList}>
-                <SkillList src={checkMarkIcon} skill="React.js"/>
-                <SkillList src={checkMarkIcon} skill="Angular"/>
-                <SkillList src={checkMarkIcon} skill="JavaScript"/>
-                <SkillList src={checkMarkIcon} skill="TailwindCSS"/>
+                <SkillList src={checkMarkIcon} skill="Data Modeling (Objects, Fields, Relationships)"/>
+                <SkillList src={checkMarkIcon} skill="Validation Rules & Record-Level Security"/>
+                <SkillList src={checkMarkIcon} skill="Developer Console & Debug Logs"/>
+                <SkillList src={checkMarkIcon} skill="Salesforce Flow Basics"/>
             </div>
             <hr />
             <div className={styles.skillList}>
-                <SkillList src={checkMarkIcon} skill="MySQL"/>
-                <SkillList src={checkMarkIcon} skill="MongoDB"/>
-                <SkillList src={checkMarkIcon} skill="SQL Server"/>
-                <SkillList src={checkMarkIcon} skill="Git & GitHub"/>
-            </div>
-            <hr />
-            <div className={styles.skillList}>
-                <SkillList src={checkMarkIcon} skill="Postman "/>
-                <SkillList src={checkMarkIcon} skill="Selenium"/>
-                <SkillList src={checkMarkIcon} skill="JUnit"/>
-                <SkillList src={checkMarkIcon} skill="Debugging Tools"/>
+                <SkillList src={checkMarkIcon} skill="Platform Development Basics"/>
+                <SkillList src={checkMarkIcon} skill="Asynchronous Apex (Future, Queueable)"/>
+                <SkillList src={checkMarkIcon} skill="Lightning Web Components"/>
+                <SkillList src={checkMarkIcon} skill="Salesforce DX & CLI"/>
             </div>
         </section>
     );    
